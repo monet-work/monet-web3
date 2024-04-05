2bc51cdf8b04cd75102ff3e1cdfc60fa2fb0cf56
arindammonet <arindam.d@monet.work>
2024-04-05 12:56:30 +0530

    fix: update ui

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,17 +1,34 @@
 "use client";
 
-import { ConnectButton, client } from "@/app/thirdweb";
+import { ConnectButton, client, elpContract } from "@/app/thirdweb";
 import { Wallet, createWallet } from "thirdweb/wallets";
 import { useMutation } from "@tanstack/react-query";
 import { createCustomer } from "@/lib/api-requests";
 import { useCustomerStore } from "@/store/customerStore";
+import { useSendTransaction } from "thirdweb/react";
+import { prepareContractCall, resolveMethod, toWei } from "thirdweb";
+import { Button } from "./ui/button";
 
 const Navbar = () => {
   const customerStore = useCustomerStore();
+  const { mutate: sendRedeemPointsTransaction, isError } = useSendTransaction();
+
+  const redeemPointsTransaction = async () => {
+    const transaction = await prepareContractCall({ 
+      contract: elpContract, 
+      method: 'OrderYourTokens', 
+      params: [0.1]
+    });
+    await sendRedeemPointsTransaction(transaction as any);
+  }
 
   const createCustomerMutation = useMutation({
     mutationFn: createCustomer
   });
+
+  const handleRedeemPoints = async() => {
+    await redeemPointsTransaction();
+  }
 
 
   const handleConnectWallet = async (wallet: Wallet) => {
@@ -49,7 +66,7 @@ const Navbar = () => {
             wallets={[createWallet("io.metamask")]}
             onConnect={handleConnectWallet}
           />
-          ;
+          <Button onClick={handleRedeemPoints}>Redeem</Button>
         </div>
       </div>
     </nav>
