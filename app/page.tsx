bf194d8bae43c5bb67daec8f0b408eb4c1c26fe0
arindammonet <arindam.d@monet.work>
2024-04-09 11:00:22 +0530

    fix: update ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,10 @@
 import CollectPoints from "@/components/collect-points";
-import { Button } from "@/components/ui/button";
-import { SparklesCore } from "@/components/ui/sparkles";
-
+import Hero from "@/components/hero";
 
 export default function Home() {
   return (
     <main className="">
-      <div className="h-screen relative w-full bg-black flex flex-col items-center justify-center overflow-hidden">
-        <div className="w-full absolute inset-0 h-screen">
-          <SparklesCore
-            id="tsparticlesfullpage"
-            background="transparent"
-            minSize={0.6}
-            maxSize={1.4}
-            particleDensity={100}
-            className="w-full h-full"
-            particleColor="#FFFFFF"
-          />
-        </div>
-        <h1 className="md:text-6xl text-2xl lg:text-6xl md:max-w-2xl font-bold text-center text-white relative z-20">
-          Unleashing points like <br/><span className="text-teal-400">never before!</span>
-        </h1>
-
-        <Button
-          className="cursor-pointer mt-4 text-primary-foreground text-xl font-light transition duration-75 hover:font-bold z-10"
-          variant={"ghost"}
-        >
-          <a href="#collect-points"> Show me how to collect points?</a>
-        </Button>
-      </div>
+      <Hero />
 
       <section
         className="py-16 px-4 dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center"
--- components/sub-navbar.tsx
+++ components/sub-navbar.tsx
@@ -2,16 +2,14 @@
 
 import { elpContract } from "@/app/thirdweb";
 import { useCustomerStore } from "@/store/customerStore";
+import { useGlitch } from "react-powerglitch";
 import {
-  estimateGas,
   prepareContractCall,
-  simulateTransaction,
-  toEther,
   toWei,
 } from "thirdweb";
 import { useSendTransaction } from "thirdweb/react";
 import { Button } from "./ui/button";
-import { use, useEffect } from "react";
+import { useEffect } from "react";
 import { toast } from "sonner";
 import { useMutation } from "@tanstack/react-query";
 import { redeemPoints } from "@/lib/api-requests";
@@ -19,6 +17,33 @@ import { redeemPoints } from "@/lib/api-requests";
 const transferFee = toWei("0.001");
 
 const SubNavbar = () => {
+
+  const glitch = useGlitch({
+    playMode: "hover",
+    createContainers: true,
+    hideOverflow: false,
+    timing: {
+      duration: 2000,
+    },
+    glitchTimeSpan: {
+      start: 0.5,
+      end: 0.7,
+    },
+    shake: {
+      velocity: 15,
+      amplitudeX: 0.2,
+      amplitudeY: 0.2,
+    },
+    slice: {
+      count: 6,
+      velocity: 15,
+      minHeight: 0.02,
+      maxHeight: 0.15,
+      hueRotate: true,
+    },
+    pulse: false,
+  });
+
   const {
     data: sendTransactionData,
     mutate: sendRedeemPointsTransaction,
@@ -76,6 +101,9 @@ const SubNavbar = () => {
             description:
               "Your on-chain points have been redeemed successfully. It will be reflected in your wallet soon.",
           });
+
+          // reloading the page as a temporary fix to update the on chain points. Ideally, thirdweb events needs to be listened and acted upon
+          window.location.reload();
         },
         onError: (error) => {
           console.error(error);
@@ -112,6 +140,7 @@ const SubNavbar = () => {
                 onClick={handleRedeemPoints}
                 className="text-sm bg-teal-900"
                 loading={isPending}
+                ref={glitch.ref}
               >
                 Redeem
               </Button>
