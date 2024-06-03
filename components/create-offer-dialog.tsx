c48edbc2cbae7e7a2be855c313a86d4708cf88c0
Nainish Rai <nainishraisjp@gmail.com>
2024-06-03 15:02:04 +0530

    added create offer dialog and useAuth fix

---
+++ components/create-offer-dialog.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { AlertDialogContent } from "./ui/alert-dialog";
+import BuyOfferComponent from "./buy-offer-component";
+import SellOfferComponent from "./sell-offer-component";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+type Props = {};
+
+function CreateOfferDialog({}: Props) {
+  const [pricePerPoint, setPricePerPoint] = React.useState(0);
+  const [collateral, setCollateral] = React.useState(0);
+  const [amount, setAmount] = React.useState(0);
+  return (
+    <AlertDialogContent>
+      <Tabs defaultValue="sell" className="w-full">
+        <TabsList className="w-full bg-neutral-900">
+          <TabsTrigger value="sell" className="w-full  p-2 text-center">
+            <div className=" w-full text-center  ">Sell</div>
+          </TabsTrigger>
+          <TabsTrigger className="w-full" value="buy">
+            <div className=" w-full text-center  ">Buy</div>
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="sell">
+          {" "}
+          <SellOfferComponent />
+        </TabsContent>
+        <TabsContent value="buy">
+          <BuyOfferComponent />{" "}
+        </TabsContent>
+      </Tabs>
+    </AlertDialogContent>
+  );
+}
+
+export default CreateOfferDialog;
