ecf96876fa7eb34ce8352717b29cda54795ad6cb
Nainish Rai <nainishraisjp@gmail.com>
2024-06-03 20:54:05 +0530

    new create offer form

--- components/forms/create-offer-form.tsx
+++ components/forms/create-offer-form.tsx
@@ -3,7 +3,17 @@ import { useForm, Form, FormProvider } from "react-hook-form";
 import { z } from "zod";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import {
   FormField,
   FormItem,
@@ -20,6 +30,7 @@ type Props = {
 
 const formSchema = z.object({
   offerType: z.string(),
+  point: z.string(),
   pricePerPoint: z.coerce.number(),
   quantity: z.string(),
   fillType: z.string(),
@@ -30,6 +41,7 @@ const CreateOfferForm: React.FC<Props> = ({ onCanceled }) => {
     resolver: zodResolver(formSchema),
     defaultValues: {
       quantity: "",
+      point: "",
       pricePerPoint: 0,
       offerType: "sell",
       fillType: "partial",
@@ -82,12 +94,37 @@ const CreateOfferForm: React.FC<Props> = ({ onCanceled }) => {
                 </FormLabel>
                 <FormControl>
                   <Input
-                    className="bg-transparent  outline-none my-1 border-none active:outline-none active:border-none text-lg focus:outline-none focus:border-none"
+                    className="bg-transparent  outline-none  border-none active:outline-none active:border-none  focus:outline-none focus:border-none"
                     placeholder="0"
                     {...field}
                   />
                 </FormControl>
                 <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="point"
+            render={({ field }) => (
+              <FormItem className="bg-neutral-900 p-4 rounded-lg">
+                <FormLabel>Select Point</FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select a POINT" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectItem value="ADB">ADB</SelectItem>
+                    <SelectItem value="TSTP">TSTP</SelectItem>
+                    <SelectItem value="NRG">NRG</SelectItem>
+                  </SelectContent>
+                </Select>
               </FormItem>
             )}
           />
@@ -101,10 +138,42 @@ const CreateOfferForm: React.FC<Props> = ({ onCanceled }) => {
                 </FormLabel>
                 <FormControl>
                   <Input
-                    className="bg-transparent  outline-none my-1 border-none active:outline-none active:border-none text-lg focus:outline-none focus:border-none"
+                    className="bg-transparent  outline-none  border-none active:outline-none active:border-none  focus:outline-none focus:border-none"
                     placeholder="0"
                     {...field}
                   />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="fillType"
+            render={({ field }) => (
+              <FormItem className="space-y-2 bg-neutral-900 p-4 rounded-lg">
+                <FormLabel>Select FillType</FormLabel>
+                <FormControl>
+                  <RadioGroup
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                    className="flex flex-col space-y-1"
+                  >
+                    <FormItem className="flex items-center space-x-3 space-y-0">
+                      <FormControl>
+                        <RadioGroupItem value="Partial" />
+                      </FormControl>
+                      <FormLabel className="font-normal">
+                        Partial Fill
+                      </FormLabel>
+                    </FormItem>
+                    <FormItem className="flex items-center space-x-3 space-y-0">
+                      <FormControl>
+                        <RadioGroupItem value="Full" />
+                      </FormControl>
+                      <FormLabel className="font-normal">Full Fill</FormLabel>
+                    </FormItem>
+                  </RadioGroup>
                 </FormControl>
                 <FormMessage />
               </FormItem>
