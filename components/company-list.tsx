c903abaa9256d053ebdc8142cff9f9bd31cfa242
arindammonet <arindam.d@monet.work>
2024-06-03 12:54:26 +0530

    fix: revert company list and comany columns

---
+++ components/company-list.tsx
@@ -0,0 +1,20 @@
+
+import { columns } from "./table-columns/company-columns";
+import { DataTable } from "./data-table/data-table";
+import { Company } from "@/models/company.model";
+
+type Props = {
+  companies: Company[];
+  loading?: boolean;
+};
+
+const CompanyList: React.FC<Props> = ({ companies, loading = false }) => {
+  return (
+    <div>
+      <h2 className="mb-4 font-semibold text-slate-600p">Companies</h2>
+      <DataTable columns={columns} data={companies} loading={loading} />
+    </div>
+  );
+};
+
+export default CompanyList;
