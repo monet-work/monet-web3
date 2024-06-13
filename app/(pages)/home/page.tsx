6be34c0c4e13f0860b497d9023368584d3bfb191
arindam-monet <arindam.dawn@monet.work>
2024-06-13 08:29:08 +0530

    feat: update landing page colours

--- app/(pages)/home/page.tsx
+++ app/(pages)/home/page.tsx
@@ -10,11 +10,11 @@ const HomePage = () => {
       <section className="bg-background text-white">
         <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
           <Spline
-            scene="https://prod.spline.design/SCXaqhx0ZDC6BNrz/scene.splinecode"
-            className="absolute left-1/2 transform -translate-x-1/2 top-1/2 -translate-y-1/2"
+            scene="https://prod.spline.design/SCXaqhx0ZDC6BNrz/scene.splinecode" 
+            className="absolute left-1/2 transform -translate-x-1/2 top-1/2 -translate-y-1/2 opacity-40"
           />
           <div className="mx-auto max-w-3xl text-center z-50">
-            <h1 className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-5xl font-extrabold text-transparent">
+            <h1 className="bg-gradient-to-r from-yellow-200 via-yellow-600 to-yellow-400 bg-clip-text text-5xl font-extrabold text-transparent">
               Revolutionizing Loyalty with
               <span className="sm:block"> Decentralized Rewards. </span>
             </h1>
@@ -27,13 +27,13 @@ const HomePage = () => {
 
             <div className="mt-8 flex flex-wrap justify-center gap-4">
               <Link href={"/company/login"}>
-                <Button className="bg-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-purple-600 hover:text-white focus:outline-none focus:ring active:text-opacity-75 sm:w-auto">
+                <Button className="bg-accent px-12 py-3 text-sm font-medium text-white hover:bg-primary hover:text-muted focus:outline-none focus:ring active:text-opacity-75 sm:w-auto">
                   Continue as Business
                 </Button>
               </Link>
 
               <Link href={"/customer/login"}>
-                <Button className="bg-slate-500 px-12 py-3 text-sm font-medium text-white hover:bg-green-600 focus:outline-none focus:ring active:bg-blue-500 sm:w-auto">
+                <Button className="bg-slate-500 px-12 py-3 text-sm font-medium text-white hover:primary hover:text-muted focus:outline-none focus:ring active:bg-primary sm:w-auto">
                   Continue as Customer
                 </Button>
               </Link>
