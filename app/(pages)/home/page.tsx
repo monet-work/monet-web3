6b535ed86ccc890583497d9fed01875e4af9a7af
Nainish Rai <nainishraisjp@gmail.com>
2024-06-20 12:54:07 +0530

    refactor: update homepage heading font size and responsiveness

--- app/(pages)/home/page.tsx
+++ app/(pages)/home/page.tsx
@@ -8,10 +8,10 @@ const HomePage = () => {
     <main>
       <FloatingConnect />
       <section className="bg-background text-white">
-        <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
+        <div className="mx-auto relative max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
           <Spline
-            scene="https://prod.spline.design/SCXaqhx0ZDC6BNrz/scene.splinecode" 
-            className="absolute left-1/2 transform -translate-x-1/2 top-1/2 -translate-y-1/2 opacity-40"
+            scene="https://prod.spline.design/SCXaqhx0ZDC6BNrz/scene.splinecode"
+            className="absolute left-1/2 transform z-10 -translate-x-1/2 top-1/2 -translate-y-1/2 opacity-40"
           />
           <div className="mx-auto max-w-3xl text-center z-50">
             <h1 className="bg-gradient-to-r from-yellow-200 via-yellow-600 to-yellow-400 bg-clip-text text-5xl font-extrabold text-transparent">
@@ -25,7 +25,7 @@ const HomePage = () => {
               redeem, and trade points.
             </p>
 
-            <div className="mt-8 flex flex-wrap justify-center gap-4">
+            <div className="mt-8 relative z-50 flex flex-wrap justify-center gap-4">
               <Link href={"/company/login"}>
                 <Button className="bg-accent px-12 py-3 text-sm font-medium text-white hover:bg-primary hover:text-muted focus:outline-none focus:ring active:text-opacity-75 sm:w-auto">
                   Continue as Business
