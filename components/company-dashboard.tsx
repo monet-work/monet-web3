6463c6a0eccd2590c414880aee6f47bd77985135
arindam-monet <arindam.dawn@monet.work>
2024-07-10 07:39:14 +0530

    fix: resolve data sync

--- components/company-dashboard.tsx
+++ components/company-dashboard.tsx
@@ -171,30 +171,6 @@ const CompanyDashboard: React.FC<Props> = ({
                   contract={companyContract}
                   isApproved={isCompanyApproved}
                 />
-                {/* <DisableBlockIfNoContract disabled={!isCompanyApproved}>
-                  <Card className="h-full">
-                    <CardHeader className="pb-2">
-                      <CardDescription>Points On Chain</CardDescription>
-                      <CardTitle className="text-4xl">
-                        0
-                      </CardTitle>
-                    </CardHeader>
-                    <CardContent>
-                      <div className="text-xs text-muted-foreground">
-                        <div className="flex flex-col">
-                          <span>
-                            <span className="font-semibold">NA</span> points
-                            available
-                          </span>
-                          <span>
-                            <span className="font-semibold">NA</span> points
-                            distributed
-                          </span>
-                        </div>
-                      </div>
-                    </CardContent>
-                  </Card>
-                </DisableBlockIfNoContract> */}
 
                 <DisableBlockIfNoContract disabled={!isCompanyApproved}>
                   <Card className="h-full">
