3aa477235a71ad2fd6957f759575df81ab6727a9
arindam-monet <arindam.dawn@monet.work>
2024-07-10 06:52:29 +0530

    fix: update navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,8 +4,17 @@ import { ConnectButton, client } from "@/app/contract-utils";
 import { createWallet } from "thirdweb/wallets";
 import { MonetWorkLogo } from "./icons/monet-work-logo";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
 
 const Navbar = () => {
+  const pathname = usePathname();
+  const isRouteActive = (route: string) => {
+    return pathname.includes(route);
+  };
+
+  const isMarketplaceRoute = isRouteActive("/marketplace");
+  const isDashboardRoute = isRouteActive("/customer/dashboard");
   return (
     <nav className="h-[70px] sticky top-0 bg-background w-full z-50">
       <div className="flex justify-between items-center h-full container text-sm">
@@ -17,14 +26,18 @@ const Navbar = () => {
             <MonetWorkLogo className="w-28" />
           </Link>
           <Link
-            href="/marketplace"
-            className="relative text-typography-white64 hover:text-typography-white"
+            href={"/customer/dashboard"}
+            className={cn("text-muted-foreground hover:text-primary", {
+              "text-primary": isDashboardRoute,
+            })}
           >
             Dashboard
           </Link>
           <Link
-            href="/marketplace"
-            className="relative text-typography-white64 hover:text-typography-white"
+            href={"/marketplace"}
+            className={cn("text-muted-foreground hover:text-primary", {
+              "text-primary": isMarketplaceRoute,
+            })}
           >
             Marketplace
           </Link>
